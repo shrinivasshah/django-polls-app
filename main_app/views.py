771ec20bb9800ff97d72a21be27e27c63508ccd8
shrinivasshah <shrinivasshah05@gmail.com>
2021-02-21 08:41:56 +0530

    get_objects_404

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.http import HttpResponse
 from .models import Question
 # Create your views here.
 
 
-# def results(request, question_id):
-#     response = "You are looking at the results of question %s." 
-#     return HttpResponse(response % question_id)
-
-# def vote(request, question_id):
-#     return HttpResponse("You're voting on question  %s." % question_id)
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'context':latest_question_list}
+    print(context)
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
@@ -21,4 +17,11 @@ def detail(request, question_id):
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'polls/details.html', {'question':question})    
+    return render(request, 'polls/detail.html', {'question':question})    
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s." 
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question  %s." % question_id)
